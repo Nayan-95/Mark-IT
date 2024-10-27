83ec4c62e0ba6c3756b0bb73b44fe9563de86c4d
Manabodha Mahananda <manmahanand95@gmail.com>
2024-10-27 18:57:19 +0530

    make changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Map from './components/map'
-import Header from './components/Header.jsx'
+import React from 'react';
+import Map from './components/map';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Map />
     </div>
   );
